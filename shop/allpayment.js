d3016b5958fb54c273a522f7d5889472633c548f
Yunitaayy <94414522+Yunitaayy@users.noreply.github.com>
2021-11-16 07:45:22 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,25 +1,14 @@
 const allpayment = (prefix) => {
 return `PAYMENT BY ZEEONE OFC
 
-*さ BANK*
-1. BRI 
-	ㅁ 5074-01-003141-52-8
-		• R***A*DI
 		
 *さ E-MONEY*
 1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
-2. OVO
-	ㅁ 0887 4350 47326
-		• EL*****TI
+	ㅁ 088239855250
+		• ni**st***
 3. DANA
 	ㅁ 0887 4350 47326
-		• ZE**** **C
-
-*さ PULSA*
-
-Tidak tersedia
+		• ni**St***
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
